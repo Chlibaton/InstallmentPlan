2ec835646ffba3c232d9de17ecca053638415f63
Charlie <charliemagnefrancis.libaton@gmail.com>
2019-07-24 16:13:26 +0800

    dev

--- resources/js/components/Layout/SidePanelComponent.vue
+++ resources/js/components/Layout/SidePanelComponent.vue
@@ -1,64 +1,65 @@
+<style scoped>
+    .sidebarBackground{
+        background: black;
+        position: absolute;
+    }
+    .navList{
+        color: white;
+        font-size: 18px !important;
+    }
+</style>
+
+
+
 <template>
-        <v-app light>       
-            <v-navigation-drawer :clipped="clipped" v-model="drawer" enable-resize-watcher app dark class="primary lighten-3">
-            <v-list>
-                <v-list-group v-for="item in items" :value="item.active" :key="item.title">
-                <v-list-tile slot="item" 
-                            :to="item.path == '#' ? '' : item.path" 
-                            :exact="item.exact" 
-                            class="yellow--text"
-                            active-class="red--text">
-                    <v-list-tile-action>
-                    <v-icon>{{ item.action }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                    <v-list-tile-title >{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                    <v-list-tile-action v-if="item.items.length > 0">
-                    <v-icon>keyboard_arrow_down</v-icon>
-                    </v-list-tile-action>
-                </v-list-tile>         
-                </v-list-group>
-            </v-list>
-            </v-navigation-drawer>      
-            <v-toolbar fixed app :clipped-left="clipped">
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title>Topics</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-                <v-icon>more_vert</v-icon>
-            </v-btn>
-            </v-toolbar>      
-            <v-content>
-            <v-container fluid>
-                <router-view></router-view>
-            </v-container>
-            </v-content>
-        </v-app>
-        
+    <v-navigation-drawer  permanent class='sidebarBackground'>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> 
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title class='navList'>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 <<script>
 export default {
-      data () {
-    return {
-      drawer: true,
-      clipped: false,
-      items: [
-        {
-          action: 'local_activity',
-          title: 'Attractions',
-          path: '/',
-          items: [],
-        },
-        {
-          action: 'restaurant',
-          title: 'Breakfast',
-          path: '/breakfast',
-          items: []
-        },       
-      ]
+    data(){
+        return {
+            items: [
+                { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+                { title: 'Photos', icon: 'mdi-image' },
+                { title: 'About', icon: 'mdi-help-box' },
+            ],
+            right: null,
+            
+        }
     }
-  }
+   
 }
 </script>
